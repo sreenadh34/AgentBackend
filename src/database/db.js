33f74b2b2000bd108224b9157c6a262750ef3ba1
Dheeraj <137279485+dheerajt112@users.noreply.github.com>
2024-09-22 03:01:00 +0530

    db link chnaged

--- src/database/db.js
+++ src/database/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI)
+    .connect("mongodb+srv://shorts:W4xRJzkZWB0SVckq@cluster0.v9gmobn.mongodb.net/eduartista")
     .then((response) => {
       console.log("MongoDB connected");
     })
